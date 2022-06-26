e97a0995bb89c66101f642adec579614962905d7
WilliamAlkawaz <77669116+WilliamAlkawaz@users.noreply.github.com>
2022-06-26 14:35:13 +1200

    Change Data

--- src/components/Data.js
+++ src/components/Data.js
@@ -2,9 +2,9 @@ const Data = {
     employee: 'Sam', 
     projects: [
         {id: -1, name: 'Select a project ...', tasks:[]},
-        {id: 0, name: 'Datacom Internal: K38125 IF0025', tasks: [{id: -1, name: 'Select a task ...'}, {id: 0, name: 'Task11'}, {id: 1, name: 'Task21'}, {id: 2, name: 'Task31'}]}, 
-        {id: 1, name: 'Datacom Internal: K38017 AKL SERVICE', tasks: [{id: -1, name: 'Select a task ...'}, {id: 3, name: 'Task12'}, {id: 4, name: 'Task22'}, {id: 5, name: 'Task32'}]}, 
-        {id: 2, name: 'Datacom Internal: K38113 IF0025 Ecosystem Customer', tasks: [{id: -1, name: 'Select a task ...'}, {id: 6, name: 'RBNZ Workday [Integration]'}, {id: 7, name: 'Rabobank Workday [Integration]'}, {id: 9, name: 'GNS/ESR Workday [Integration]'}, {id: 8, name: 'Transition [Integration]'}]}
+        {id: 0, name: 'My Company Internal: P39744 IT0024', tasks: [{id: -1, name: 'Select a task ...'}, {id: 0, name: 'Task11'}, {id: 1, name: 'Task21'}, {id: 2, name: 'Task31'}]}, 
+        {id: 1, name: 'My Company Internal: P38933 AKL SERVICE', tasks: [{id: -1, name: 'Select a task ...'}, {id: 3, name: 'Task12'}, {id: 4, name: 'Task22'}, {id: 5, name: 'Task32'}]}, 
+        {id: 2, name: 'My Company Internal: P39033 IT0024 API Customer', tasks: [{id: -1, name: 'Select a task ...'}, {id: 6, name: 'CompanyA Workday [Integration]'}, {id: 7, name: 'CompanyB Workday [Integration]'}, {id: 9, name: 'TFC/ITO Workday [Integration]'}, {id: 8, name: 'Transition [Integration]'}]}
     ], 
     workType: [
         {id: -1, name: 'Select a work type ...'},
